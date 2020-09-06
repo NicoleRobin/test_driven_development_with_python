092a7c35e47d386147ccbfd9a2670f61c7b9e6f0
nicolerobin <lit050528@gmail.com>
2020-09-06 23:12:09 +0800

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -11,4 +11,10 @@ def home_page(request):
         return redirect('/lists/the-only-list-in-the-world/')
 
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
+
+
+@csrf_exempt
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
