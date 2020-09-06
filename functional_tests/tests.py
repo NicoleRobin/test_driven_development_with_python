1320037eab6a4434aca8463a633e2b03902d26dc
nicolerobin <lit050528@gmail.com>
2020-09-06 22:51:38 +0800

    add check_for_row_in_list_table

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,6 +16,11 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊丽丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
@@ -37,13 +42,12 @@ class NewVisitorTest(LiveServerTestCase):
         # 伊迪斯的爱好是使用假蝇做鱼饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
 
-        # 她按回车键后，页面更新了
-        # 待办事项表格显示了：'1：Buy peacock feathers'
+        # 她按回车键后，被带到了一个新URL
+        # 这个页面的待办事项清单中显示：'1：Buy peacock feathers'
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1：Buy peacock feathers', [row.text for row in rows])
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1：Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了'Use peacock feathers to make a fly'（使用孔雀羽毛做假蝇）
@@ -61,4 +65,41 @@ class NewVisitorTest(LiveServerTestCase):
             [row.text for row in rows],
         )
 
-        self.fail('Finish the test!')
+        # 伊迪斯想知道这个网站是否会记住它的清单
+
+        # 页面再次更新，她的清单中显示了这两个待办事项
+        self.check_for_row_in_list_table('2：Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1：Buy peacock feathers')
+
+        # 现在一个叫弗朗西斯的新用户访问了网站
+
+        ## 我们使用一个新浏览器会话
+        ## 确保依迪丝的信息不会从cookie中泄露出来
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # 弗朗西斯访问首页
+        # 页面中看不到依迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # 弗朗西斯输入一个新待办事项，新建一个清单
+        # 他不像依迪丝那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 弗朗西斯获得了他的唯一URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 这个页面还是没有依迪丝的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 两个人都很满意，去睡觉了
+
